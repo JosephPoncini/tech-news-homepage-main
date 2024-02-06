6e0511d2ec047b451e20dce1756811dada2be944
JosephPoncini <jxponcini@gmail.com>
2024-02-06 15:00:44 -0800

    pre-peer review commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,25 @@ module.exports = {
       "Inter-ExtraBold": "Inter-ExtraBold",
       "Inter-Regular": "Inter-Regular",
     },
-    extend: {},
+
+
+
+    extend: {
+      colors: {
+        "Off-white": "#fffdfa",
+        "Grayish-blue": "#c5c6ce",
+        "Dark-grayish-blue": "#5d5f79",
+        "Very-dark-blue": "#00001a",
+
+        "Soft-orange": "#e9ab53",
+        "Soft-red": "#f15e50",
+    },
+
+    fontSize: {
+      "body": "15px"
+    },
   },
+},
   plugins: [
     require('flowbite/plugin')
   ]
